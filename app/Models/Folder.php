009378cadb123ad5e0bc7c15714c8ca689626ff2
Florian <palabostg@gmail.com>
2024-01-16 14:14:06 +0100

    refactor(Folder): add relations & fillable properties

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -5,9 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Folder extends Model
 {
     use HasFactory, HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'parent_id',
+        'created_by',
+    ];
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Folder::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(Folder::class, 'parent_id');
+    }
 }
