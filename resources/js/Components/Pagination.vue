6ea739d77f8afbb79f4ab49987b6703cf7ecc1e6
Florian <palabostf@gmail.com>
2024-01-18 16:15:26 +0100

    feat: add Pagination component

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps({
+    links: Array,
+});
+const links = props.links;
+debugger;
+</script>
+<template>
+    <div v-if="links.length > 0">
+        <div class="flex flex-wrap mt-8">
+            <template v-for="(link, key) in links" :key="key">
+                <div
+                    v-if="link.url === null"
+                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
+                    v-html="link.label"
+                />
+
+                <Link
+                    v-else
+                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-white focus:border-primary focus:text-primary"
+                    :class="{ 'bg-white': link.active }"
+                    :href="link.url"
+                    v-html="link.label"
+                />
+            </template>
+        </div>
+    </div>
+</template>
