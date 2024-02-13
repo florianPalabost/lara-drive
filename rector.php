4410128ffc85114590331e3cf8f0a7d76b21de16
Florian <palabostg@gmail.com>
2024-02-13 15:28:34 +0100

    chore(rector): update config

--- rector.php
+++ rector.php
@@ -6,17 +6,13 @@ use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
 use Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector;
-use Rector\Set\ValueObject\SetList;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/app',
-        __DIR__ . '/bootstrap',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
+        __DIR__ . '/database',
         __DIR__ . '/tests',
     ]);
 
