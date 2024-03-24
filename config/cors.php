fcdaf7460626d0c7d655eb22dedcb7e5ded4c981
Florian <palabostf@gmail.com>
2024-03-24 12:34:02 +0100

    feat(api): add scramble + udate libs

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
+    'paths'                    => ['api/*', 'sanctum/csrf-cookie', 'docs/*'],
 
     'allowed_methods'          => ['*'],
 
