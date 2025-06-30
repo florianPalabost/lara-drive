916feb43170cdbe7f1e4413381fabd24f8cc81fb
FlorianPalabost <palabostf@gmail.com>
2025-06-30 22:45:21 +0200

    chore: add folder routes + clean

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,8 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\FolderController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -28,10 +15,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('folders', FolderController::class);
 });
 
 require __DIR__ . '/settings.php';
