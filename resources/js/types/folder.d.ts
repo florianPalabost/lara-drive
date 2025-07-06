d71ef3ee7f1a623c0589570837e93ce4584fc388
FlorianPalabost <palabostf@gmail.com>
2025-07-06 10:57:40 +0200

    chore: add drive file type

--- resources/js/types/folder.d.ts
+++ resources/js/types/folder.d.ts
@@ -6,4 +6,17 @@ export interface Folder {
     created_at: string;
     updated_at: string;
     children?: Array<Folder>;
+    files?: Array<DriveFile>;
+}
+
+export interface DriveFile {
+    id: number;
+    uuid: string;
+    folder_id: number;
+    original_name: string;
+    mime_type: string;
+    size: number;
+    path: string;
+    created_at: string;
+    updated_at: string;
 }
