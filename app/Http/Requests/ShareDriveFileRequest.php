5de0c5cf53bcf280e1770dce198df330eb72198a
FlorianPalabost <palabostf@gmail.com>
2025-07-14 11:15:56 +0200

    feat(files/share): add file share request

---
+++ app/Http/Requests/ShareDriveFileRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use App\Enums\DriveFilePermissionEnum;
+use App\Enums\DriveFileShareExpiresAtEnum;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShareDriveFileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'user_email' => ['nullable', 'exists:users,email'],
+            'expires_in' => ['nullable', 'in:' . collect(DriveFileShareExpiresAtEnum::cases())->map(fn (DriveFileShareExpiresAtEnum $permission) => $permission->value)->implode(',')],
+            'permission' => ['required', 'in:' . collect(DriveFilePermissionEnum::cases())->map(fn (DriveFilePermissionEnum $permission) => $permission->value)->implode(',')],
+        ];
+    }
+}
