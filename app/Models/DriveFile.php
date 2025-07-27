989e99a33d65c0677ddb887243c5f570c34c18af
FlorianPalabost <palabostf@gmail.com>
2025-07-27 15:40:44 +0200

    chore: order version desc

--- app/Models/DriveFile.php
+++ app/Models/DriveFile.php
@@ -55,7 +55,7 @@ class DriveFile extends Model
      */
     public function versions(): HasMany
     {
-        return $this->hasMany(DriveFileVersion::class);
+        return $this->hasMany(DriveFileVersion::class)->orderBy('version', 'desc');
     }
 
     /**
