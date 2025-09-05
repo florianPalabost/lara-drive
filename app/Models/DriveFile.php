8a66b771a29a211f9ea106e2dc2ed6e51017c891
FlorianPalabost <palabostf@gmail.com>
2025-09-05 12:28:54 +0200

    feat: add soft delete to model & migrations

--- app/Models/DriveFile.php
+++ app/Models/DriveFile.php
@@ -10,11 +10,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DriveFile extends Model
 {
     /** @use HasFactory<\Database\Factories\DriveFileFactory> */
-    use HasFactory, HasUuidColumn;
+    use HasFactory, HasUuidColumn, SoftDeletes;
 
     protected $fillable = [
         'uuid',
--- app/Models/DriveFileVersion.php
+++ app/Models/DriveFileVersion.php
@@ -7,11 +7,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DriveFileVersion extends Model
 {
     /** @use HasFactory<\Database\Factories\DriveFileVersionFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /** @var list<string> */
     protected $fillable = [
