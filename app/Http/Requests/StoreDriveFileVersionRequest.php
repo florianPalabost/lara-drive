b9fa7b031e7b5a0378bf6a7872744e6c0dc720c4
FlorianPalabost <palabostf@gmail.com>
2025-09-07 14:40:30 +0200

    chore: apply ValidationRules alias (phpstan)

--- app/Http/Requests/StoreDriveFileVersionRequest.php
+++ app/Http/Requests/StoreDriveFileVersionRequest.php
@@ -19,7 +19,7 @@ class StoreDriveFileVersionRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return ValidationRules
      */
     public function rules(): array
     {
